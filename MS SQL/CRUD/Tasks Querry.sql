d77f5790f7761c3120fd89640b0d5d76e689dce7
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-09-03 23:05:41 +0300

    Exercises

---
+++ MS SQL/CRUD/Tasks Querry.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Departments   -- TASK 2
+
+SELECT [NAME] FROM Departments -- TAKS 3
+
+SELECT FirstName, LastName, Salary FROM Employees -- TASK 4
+
+SELECT FirstName, MiddleName, LastName FROM Employees -- TASK 5
+
+-- TASK 6
+
+SELECT DISTINCT Salary FROM Employees -- TASK 7
+
+SELECT * FROM Employees
+	WHERE JobTitle='Sales Representative' -- TASK 8
