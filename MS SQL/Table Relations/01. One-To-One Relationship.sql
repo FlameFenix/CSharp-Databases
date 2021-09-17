7ed93d5f606a4891f4a117562d2c26c9bde1b5fc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-09-17 22:04:51 +0300

    Exercises

---
+++ MS SQL/Table Relations/01. One-To-One Relationship.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Practice
+
+USE Practice
+
+CREATE TABLE Passports(
+	[PassportID] INT PRIMARY KEY IDENTITY(101, 1) NOT NULL,
+	[PassportNumber] NVARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Persons(
+	[PersonID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[FirstName] NVARCHAR(20) NOT NULL,
+	[Salary] DECIMAL(7,2),
+	[PassportID] INT FOREIGN KEY REFERENCES Passports([PassportID])
+)
+
+INSERT INTO Passports
+VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO Persons
+VALUES
+('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
