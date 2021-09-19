9538d800c81b103242d6c4a763318bdc61614d96
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-09-20 00:16:05 +0300

    Exercises

---
+++ MS SQL/Table Relations/06. University Database.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE University
+
+USE University
+
+CREATE TABLE Majors(
+	[MajorID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(20)
+)
+
+CREATE TABLE Students(
+	[StudentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[StudentNumber] NVARCHAR(20) NOT NULL,
+	[StudentName] NVARCHAR(20) NOT NULL,
+	[MajorID] INT FOREIGN KEY REFERENCES Majors([MajorID]),
+)
+
+CREATE TABLE Payments(
+	[PaymentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[PaymentDate] DATE NOT NULL,
+	[PaymentAmount] DECIMAL(10,2),
+	[StudentID] INT FOREIGN KEY REFERENCES Students([StudentID])
+)
+
+
+CREATE TABLE Subjects(
+	[SubjectID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[SubjectName] NVARCHAR(20)
+)
+
+CREATE TABLE Agenda(
+	[StudentID] INT FOREIGN KEY REFERENCES Students([StudentID]),
+	[SubjectID] INT FOREIGN KEY REFERENCES Subjects([SubjectID]),
+	PRIMARY KEY ([StudentID], [SubjectID])
+)
