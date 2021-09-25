b71236f4e4e75d30c5acaa7bdf7ff20d88b490fc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-09-25 11:19:14 +0300

    Update

--- MS SQL/Built-in Functions/14. Games From 2011 and 2012 Year.sql
+++ MS SQL/Built-in Functions/14. Games From 2011 and 2012 Year.sql
@@ -1,3 +1,3 @@
 SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS [Start] FROM Games
-	WHERE YEAR([Start]) >= 2011 AND YEAR([Start]) <= 2012
+	WHERE YEAR([Start]) BETWEEN 2011 AND 2012
 	ORDER BY [Start], [Name] ASC
