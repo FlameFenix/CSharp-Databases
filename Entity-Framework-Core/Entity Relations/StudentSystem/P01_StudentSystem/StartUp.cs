5662dd89149a76d263369e3292f5f9860da0bd81
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-11-03 00:37:18 +0200

    In Progress

---
+++ Entity-Framework-Core/Entity Relations/StudentSystem/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StudentSystemContext db = new StudentSystemContext();
+
+            db.Database.EnsureDeleted();
+
+            db.Database.EnsureCreated();
+
+            Console.WriteLine("created suc");
+
+            //db.Database.EnsureDeleted();
+        }
+    }
+}
