6d0874f408fe1ce189e263ca6d9d9f349184ff9f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-11-17 00:15:58 +0200

    Exercises

---
+++ Entity-Framework-Core/JavaScript Object Notation - JSON/CarDealer/CarDealer/DTO/InputCarsDto.cs
@@ -0,0 +1,21 @@
+﻿using CarDealer.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CarDealer.DTO
+{
+    public class InputCarsDto
+    {
+        public InputCarsDto()
+        {
+            partsId = new List<int>();
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int TravelledDistance { get; set; }
+
+        public List<int> partsId { get; set; }
+    }
+}
--- Entity-Framework-Core/JavaScript Object Notation - JSON/CarDealer/CarDealer/StartUp.cs
+++ Entity-Framework-Core/JavaScript Object Notation - JSON/CarDealer/CarDealer/StartUp.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
 using CarDealer.Datasets;
+using CarDealer.DTO;
 using CarDealer.Models;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -41,13 +42,25 @@ namespace CarDealer
 
             var inputParts = File.ReadAllText(@"../../../Datasets/parts.json");
 
-            Console.WriteLine(ImportSuppliers(context, inputParts));
+            Console.WriteLine(ImportParts(context, inputParts));
 
             // 10. Import Cars
 
             var inputCars = File.ReadAllText(@"../../../Datasets/cars.json");
-
+           
             Console.WriteLine(ImportCars(context, inputCars));
+
+            // 11. Import Customers
+
+            var inputCustomers = File.ReadAllText(@"../../../Datasets/customers.json");
+
+            Console.WriteLine(ImportCustomers(context, inputCustomers));
+
+            // 12. Import Sales
+
+            var inputSales = File.ReadAllText(@"../../../Datasets/sales.json");
+
+            Console.WriteLine(ImportSales(context, inputSales));
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
@@ -83,7 +96,64 @@ namespace CarDealer
         {
             var cars = context.Cars;
 
+            var carsJson = JsonConvert.DeserializeObject<IEnumerable<InputCarsDto>>(inputJson);
+
+            foreach (var car in carsJson)
+            {
+                Car currentCar = new Car()
+                {
+                    Make = car.Make,
+                    Model = car.Model,
+                    TravelledDistance = car.TravelledDistance,
+                };
+
+                foreach (var carPart in car.partsId.Distinct())
+                {
+                    Part partCar = context.Parts.FirstOrDefault(x => x.Id == carPart);
+
+                    if(partCar == null)
+                    {
+                        continue;
+                    }
+
+                    currentCar.PartCars.Add(new PartCar()
+                    {
+                        Part = partCar
+                    });
+                }
+
+                cars.Add(currentCar);
+            }
+
+            context.SaveChanges();
+
             return $"Successfully imported {context.Cars.Count()}.";
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            var customers = context.Customers;
+
+            var customersJson = JsonConvert.DeserializeObject<IEnumerable<Customer>>(inputJson);
+
+            customers.AddRange(customersJson);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Count()}.";
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            var sales = context.Sales;
+
+            var salesJson = JsonConvert.DeserializeObject<IEnumerable<Sale>>(inputJson);
+
+            sales.AddRange(salesJson);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Count()}.";
         }
     }
 }
